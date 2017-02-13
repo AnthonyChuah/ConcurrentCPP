5018de83539027cf2373253442f4d7bc03fd08c9
Anthony Chuah <anthony.w.chuah@gmail.com>
2017-02-13 09:22:29 +0000

    Tests

--- gp/TestQueue.cpp
+++ gp/TestQueue.cpp
@@ -1,5 +1,7 @@
 #include "TestQueue.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 TestQueue::TestQueue(int _size) {
   data.reserve(_size);
@@ -10,12 +12,14 @@ void TestQueue::insertInt(int _val) {
   data.emplace_back(_val);
   std::cout << "Inserted int into queue: " << _val << "\n";
   locker.unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
   cond.notify_one();
 }
 
 int TestQueue::extractInt() {
   std::unique_lock<std::mutex> locker(mutex1);
   while (data.empty()) {
+    std::cout << "Empty queue, waiting.\n";
     cond.wait(locker);
   }
   int result = data.back();
--- gp/test.cpp
+++ gp/test.cpp
@@ -2,22 +2,26 @@
 #include <thread>
 #include <iostream>
 #include <mutex>
-
-void thread1(TestQueue& _tq, int _reps) {
-  for (int i = 0; i < _reps; ++i) {
-    std::this_thread::sleep_for(chrono::milliseconds(5));
-    _tq.insertInt(i);
-  }
-}
+#include <chrono>
 
 int main() {
   int reps = 100;
   TestQueue tq(128);
-  std::thread t1(thread1, std::ref(tq), reps);
+  auto lambda = [](TestQueue& _tq, int _reps) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    for (int i = 0; i < _reps; ++i) {
+      _tq.insertInt(i);
+    }
+  };
+  std::thread t1(lambda, std::ref(tq), reps);
+  int received[100];
   for (int i = 0; i < reps; ++i) {
-    std::this_thread::sleep_for(chrono::milliseconds(5));
-    int received = tq.extractInt();
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    received[i] = tq.extractInt();
   }
   t1.join();
+  for (int i = 0; i < reps; ++i) {
+    std::cout << received[i] << " ";
+  }
   return 0;
 }
